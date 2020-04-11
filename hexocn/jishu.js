cc040dd4fe277f3a123d6f32d7d4bbe629fe21fa
huanhao <2077780946@qq.com>
2020-04-11 18:23:23 +0800

    new

---
+++ hexocn/jishu.js
@@ -0,0 +1,14 @@
+
+<script>
+fetch('https://counts.42cloud.cn/api/counts.html')
+    .then(response => response.json())
+    .then(data => {
+        if (document.getElementById("readcount")) {
+            document.getElementById("readcount").innerHTML=data.Count;
+        }
+        if (document.getElementById("allcount")){
+            document.getElementById("allcount").innerHTML=data.Allcount;
+          }
+    })
+    .catch(err => console.error(err))
+</script>
